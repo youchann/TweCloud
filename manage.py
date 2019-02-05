1387303d15cd184a74045cb1c4fb3184d80a19d5
yutaro1031 <yutaro50@gmail.com>
2019-02-05 12:34:37 +0900

    ポート番号修正

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-
 from application import app
+import os
 
-app.run(host='127.0.0.1', port=5000, debug=True)
+port = int(os.environ.get('PORT', 5000))
 
+if __name__ == '__main__':
+    app.run(port=port)
